9cf6f8d4a085c30113691898ff73596114a75f2b
Alex Osipenko <yokumora@gmail.com>
2020-11-16 14:45:44 +0300

    1

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import 'core-js/es6/map';
 import 'core-js/es6/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import connect from '@vkontakte/vkui-connect';
+import bridge from '@vkontakte/vk-bridge';
 import App from './App';
+import connect from '@vkontakte/vkui-connect';
 // import registerServiceWorker from './sw';
 
 // Init VK App
 connect.send('VKWebAppInit', {});
-
 // Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
 // расскомментируйте строку с registerServiceWorker();
 // Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
